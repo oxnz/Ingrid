91a0680c77b9a1ea0ce66be8299e4a0ef98dd008
Max Zhang <max.zhang@airbnb.com>
2019-08-20 19:09:34 +0800

    scala mig

--- src/main/java/io/github/oxnz/Ingrid/Application.java
+++ src/main/java/io/github/oxnz/Ingrid/Application.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import com.fasterxml.jackson.module.scala.DefaultScalaModule;
 import io.github.oxnz.Ingrid.article.Article;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,6 +31,7 @@ public class Application {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper()
+                .registerModule(new DefaultScalaModule())
                 .registerModule(new ParameterNamesModule())
                 .registerModule(new Jdk8Module())
                 .registerModule(new JavaTimeModule()); // new module, NOT JSR310Module
