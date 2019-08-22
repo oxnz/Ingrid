ca4494d5804eaa53925a35d94081a8e865183910
Max Zhang <max.zhang@airbnb.com>
2019-08-22 15:22:08 +0800

    add nashornsrc/main/java/io/github/oxnz/Ingrid/nashorn/NashornService.java

---
+++ src/main/java/io/github/oxnz/Ingrid/alg/LeetCode.java
@@ -0,0 +1,4 @@
+package io.github.oxnz.Ingrid.alg;
+
+public class LeetCode {
+}
---
+++ src/test/java/io/github/oxnz/Ingrid/alg/LeetCodeTest.java
@@ -0,0 +1,12 @@
+package io.github.oxnz.Ingrid.alg;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LeetCodeTest {
+
+    @Test
+    public void topKFrequent() {
+    }
+}
