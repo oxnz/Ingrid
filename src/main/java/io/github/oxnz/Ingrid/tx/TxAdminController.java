a41dd092a4812f0e0a98e56a2a8905545b8f6d05
云心逸 <yunxinyi+github@gmail.com>
2019-08-27 23:32:57 +0800

    scala mig TxEvent

--- src/main/java/io/github/oxnz/Ingrid/tx/TxAdminController.java
+++ src/main/java/io/github/oxnz/Ingrid/tx/TxAdminController.java
@@ -1,7 +1,6 @@
 package io.github.oxnz.Ingrid.tx;
 
 import io.github.oxnz.Ingrid.tx.mq.RedisMessageProducer;
-import io.github.oxnz.Ingrid.tx.mq.TxEvent;
 import io.micrometer.core.annotation.Timed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
