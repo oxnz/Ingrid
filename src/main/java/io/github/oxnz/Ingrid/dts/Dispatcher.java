1377e193735c9521d0db1a6d6497c536ec7232f8
Max Zhang <max.zhang@airbnb.com>
2019-08-27 10:51:33 +0800

    dispatcher impl

--- src/main/java/io/github/oxnz/Ingrid/dts/Dispatcher.java
+++ src/main/java/io/github/oxnz/Ingrid/dts/Dispatcher.java
@@ -2,12 +2,15 @@ package io.github.oxnz.Ingrid.dts;
 
 import io.github.oxnz.Ingrid.dts.data.TxRecord;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class Dispatcher {
+
+    private final Map<String, Set<DestSpec>> dispatches = new HashMap<>();
 
     public List<DestSpec> dispatch(TxRecord record) {
         List<DestSpec> destSpecs = new ArrayList<>();
@@ -15,4 +18,32 @@ public class Dispatcher {
         return destSpecs;
     }
 
+    private String dispatchKey(String state, String city) {
+        if (state == null) state = "";
+        if (city == null) city = "";
+        return String.join("/", state, city);
+    }
+
+    private Set<String> dispatchKeys(String state, String city) {
+        Set<String> keys = new HashSet<>();
+        keys.add(dispatchKey(null, null));
+        keys.add(dispatchKey(state, null));
+        keys.add(dispatchKey(state, city));
+        return keys;
+    }
+
+    public void register(DestSpec destSpec) {
+        Class<? extends DestSpec> clazz = destSpec.getClass();
+        Assert.isTrue(clazz.isAnnotationPresent(Region.class), "region annotation is missing");
+        Region region = clazz.getAnnotation(Region.class);
+        String key = dispatchKey(region.state(), region.city());
+        if (!dispatches.containsKey(key))
+            dispatches.put(key, new HashSet<>());
+        dispatches.get(key).add(destSpec);
+    }
+
+    public Set<DestSpec> dispatch(String state, String city) {
+        Set<String> keys = dispatchKeys(state, city);
+        return keys.stream().map(dispatches::get).flatMap(Set::stream).collect(Collectors.toSet());
+    }
 }
---
+++ src/test/java/io/github/oxnz/Ingrid/dts/DispatcherTest.java
@@ -0,0 +1,38 @@
+package io.github.oxnz.Ingrid.dts;
+
+import org.junit.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
+
+public class DispatcherTest {
+
+    @Test
+    public void dispatch() {
+        Map<String, Set<String>> dispatches = new HashMap<>();
+        Set<String> allDispatches = new HashSet<>();
+        allDispatches.add("all-1");
+        allDispatches.add("all-2");
+        Set<String> stateDispatches = new HashSet<>();
+        stateDispatches.add("state-1");
+        stateDispatches.add("state-2");
+        stateDispatches.add("state-3");
+        Set<String> cityDisps = new HashSet<>();
+        cityDisps.add("city-1");
+        cityDisps.add("city-2");
+        dispatches.put("/", allDispatches);
+        dispatches.put("ca/", stateDispatches);
+        dispatches.put("ca/sf", cityDisps);
+        Set<String> keys = new HashSet<>();
+        keys.add("/");
+        keys.add("ca/");
+        keys.add("ca/sf");
+        List<String> disps = keys.stream().map(dispatches::get)
+                .flatMap(Set::stream)
+                .collect(Collectors.toList());
+        System.out.println(disps);
+    }
+}
