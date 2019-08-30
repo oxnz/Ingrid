617575dabe8fe1e788cad4efb684d13dd2ae6659
云心逸 <yunxinyi+github@gmail.com>
2019-08-30 23:12:08 +0800

    scala syntax refine

--- src/main/scala/io/github/oxnz/Ingrid/tx/TxHttpExecutorConfig.scala
+++ src/main/scala/io/github/oxnz/Ingrid/tx/TxHttpExecutorConfig.scala
@@ -7,11 +7,8 @@ import scala.beans.BeanProperty
 
 @Component
 @ConfigurationProperties(prefix = "tx.http.executor")
-class TxHttpExecutorConfig(@BeanProperty var timeoutMillis: Int, @BeanProperty var maxConn: Int, @BeanProperty var maxConnPerRoute: Int, @BeanProperty var keepAliveTimeoutMillis: Int, @BeanProperty var workerCount: Int) {
+class TxHttpExecutorConfig(@BeanProperty var timeoutMillis: Int = 10 * 1000, @BeanProperty var maxConn: Int = 8 * 8, @BeanProperty var maxConnPerRoute: Int = 8, @BeanProperty var keepAliveTimeoutMillis: Int = 30 * 1000, @BeanProperty var workerCount: Int = 8) {
   require(workerCount <= maxConn, "extra worker would block by insufficient conn")
-  def this() = {
-    this(10*1000, 8 * 8, 8, 30*1000, 8)
-  }
 
   override def toString = s"TxHttpExecutorConfig(timeoutMS=$timeoutMillis, maxConn=$maxConn, maxConnPerRoute=$maxConnPerRoute, keepAliveMS=$keepAliveTimeoutMillis, workerCount=$workerCount)"
 }
