3c3dab65adbdf8c0030fda02da6c65d096978e68
云心逸 <yunxinyi+github@gmail.com>
2019-09-01 00:31:57 +0800

    Create Buffer.scala

---
+++ src/main/scala/io/github/oxnz/Ingrid/ds/Buffer.scala
@@ -0,0 +1,19 @@
+package io.github.oxnz.Ingrid.ds
+
+abstract class Buffer {
+  type T
+  val element: T
+}
+
+abstract class SeqBuffer extends Buffer {
+  type U
+  type T <: Seq[U]
+  def length = element.length
+}
+
+class IPv4Buffer extends SeqBuffer {
+  type IPv4 = Int
+  override type U = IPv4
+  override type T = List[U]
+  override val element = List[IPv4](1)
+}
