82446fbe68d0eeeb2194309fa4b46ab7ed6b6b44
Max Zhang <max.zhang@airbnb.com>
2019-09-03 10:15:24 +0800

    Try -> Success/Failure

--- src/main/scala/io/github/oxnz/Ingrid/cx/CxExecutor.scala
+++ src/main/scala/io/github/oxnz/Ingrid/cx/CxExecutor.scala
@@ -2,7 +2,6 @@ package io.github.oxnz.Ingrid.cx
 
 trait CxExecutor {
 
-  @throws[CxException]
   def execute(request: CxRequest): CxResponse
 
 }
--- src/main/scala/io/github/oxnz/Ingrid/cx/CxService.scala
+++ src/main/scala/io/github/oxnz/Ingrid/cx/CxService.scala
@@ -5,6 +5,8 @@ import io.micrometer.core.instrument.MeterRegistry
 import org.slf4j.{Logger, LoggerFactory}
 import org.springframework.stereotype.Service
 
+import scala.util.{Failure, Success, Try}
+
 @Service
 class CxService(private final val metrics: MeterRegistry, executors: List[CxExecutor]) {
   final private[cx] val log: Logger = LoggerFactory.getLogger(getClass)
@@ -12,24 +14,26 @@ class CxService(private final val metrics: MeterRegistry, executors: List[CxExec
   private final val dispatches: Map[TxCategory, CxExecutor] =
     executors.groupBy(dispatchKey).map { case (k, v) => (k, v.head) }
 
+  @throws[CxException]
   def process(request: CxRequest): CxResponse = {
     require(request != null, "request should not be null")
-    var response = execute(request)
+    try {
+      execute(request)
+    } catch {
+      case e: Exception => throw new CxException(e)
+    }
+  }
+
+  private def execute(request: CxRequest): CxResponse = {
+    var executor = dispatch(request)
+    var response: CxResponse = Try(executor.execute(request)) match {
+      case Success(resp) => resp
+      case Failure(exception) => new CxResponse(false, exception.getMessage)
+    }
     metrics.counter(MetricsNames.Counter,
       String.valueOf(request.cat),
       String.valueOf(response.succ)).increment()
     response
-  }
-
-  private def execute(request: CxRequest): CxResponse = {
-    try {
-      var executor = dispatch(request)
-      executor.execute(request)
-    } catch {
-      case e: Exception =>
-        log.error("execute", e)
-        new CxResponse(false, null)
-    }
   }
 
   private def dispatch(request: CxRequest): CxExecutor = {
