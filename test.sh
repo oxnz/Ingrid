387aff6e294e79730220044cb7dde9b8b3437f5d
Max Zhang <max.zhang@airbnb.com>
2019-11-10 16:10:54 +0800

    add test.sh

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+baseURL='localhost:8080/tx'
+recordURL="$baseURL/record"
+state='never-land'
+city='lost-city'
+curl -X POST -H 'Content-Type: application/json' "$recordURL" -d @- <<EOF
+{
+	"cat":"ADDRESS",
+	"ref":1234,
+	"state":"$state",
+	"city":"$city"
+}
+EOF
+echo "RECORDS"
+curl localhost:8080/tx/records
+echo "RESULTS"
+curl localhost:8080/tx/results
